9d774499c1794d23cbbf911a685d58cdbeeb4a1d
Louis Low <iamlouislow@gmail.com>
2022-08-11 15:26:16 +0800

    set default `4k` screen

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ const sizereport = require('gulp-sizereport')
 const sourcemaps = require('gulp-sourcemaps')
 const stringReplace = require('gulp-replace')
 
-const newVersion = '1.0.0'
+const newVersion = '1.0.1'
 const distCssPath = 'dist'
 
 /*
